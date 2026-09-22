6dceadf84605719070adb2b8f51aa8a8a7a7d51a
withshk <withshk0527@gmail.com>
2026-09-22 19:12:55 +0900

    initial commit

---
+++ src/main/java/com/flagwith/flagwith/FlagWithApplication.java
@@ -0,0 +1,13 @@
+package com.flagwith.flagwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagWithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlagWithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flagwith/flagwith/FlagWithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flagwith.flagwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagWithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
